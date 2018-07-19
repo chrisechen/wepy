4836efba9c33015af3e652ca233cfff69e06b46f
chrisechen <penggoodlu521@163.com>
2018-07-19 20:54:25 +0800

    加入wx-charts

--- src/api/api.js
+++ src/api/api.js
@@ -4,7 +4,7 @@ import {wxRequest} from '../utils/wxRequest';
 //获取列表
 const getList = (params) => wxRequest(params, 'https://www.easy-mock.com/mock/5a90d25e81763562832917ab/myTest/homeList');
 //获取首页交易总额
-const getAllMoney = (params) => wxRequest(params, 'http://www.zalljinfu.com/applyInformation/selectLoanAmount.do');
+const getAllMoney = (params) => wxRequest(params, 'https://www.zalljinfu.com/applyInformation/selectLoanAmount.do');
 
 module.exports = {
   getList,
